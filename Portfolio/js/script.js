7842b7d8526b199859fd429bca05ed35d45b46bb
HEHvidunge <hh222zd@student.lnu.se>
2023-12-28 00:52:26 +0100

    wednesday dec27

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,60 +1,100 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const aboutMeBtn = document.getElementById('aboutMeBtn');
+  const landscapesBtn = document.getElementById('landscapesBtn');
+  const portraitsBtn = document.getElementById('portraitsBtn');
 
-      let currentSection = 'about-me';
-  
-      function showSection(section) {
-        document.getElementById(currentSection).classList.remove('active-section');
-        document.getElementById(section).classList.add('active-section');
-        currentSection = section;
+  const gallerySection = document.getElementById('gallerySection');
+
+  aboutMeBtn.addEventListener('click', function () {
+    showSection('aboutMeSection');
+  });
+
+  landscapesBtn.addEventListener('click', function () {
+    showSection('gallerySection');
+    loadGallery('data/landscapes.json', gallerySection);
+  });
+
+  portraitsBtn.addEventListener('click', function () {
+    showSection('gallerySection');
+    loadGallery('data/portraits.json', gallerySection);
+  });
+
+  // Initially show about me section
+  showSection('aboutMeSection');
+});
+
+function showSection(sectionId) {
+  const sections = ['aboutMeSection', 'gallerySection'];
+  sections.forEach(section => {
+    const element = document.getElementById(section);
+    if (section === sectionId) {
+      element.classList.add('active-section');
+    } else {
+      element.classList.remove('active-section');
+    }
+  });
+}
+
+function loadGallery(jsonFile, container) {
+  fetch(jsonFile)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
-  
-      function loadGallery(category) {
-        const galleryContainer = document.getElementById('gallery');
-        galleryContainer.innerHTML = ''; // Clear previous content
-  
-        fetch('photos.json')
-          .then(response => response.json())
-          .then(data => {
-            const photos = data[category];
-  
-            photos.forEach((photo, index) => {
-              const galleryItem = document.createElement('div');
-              galleryItem.className = 'gallery-item';
-              galleryItem.style.display = index === 0 ? 'block' : 'none';
-  
-              const img = document.createElement('img');
-              img.src = photo.url;
-              img.alt = photo.description;
-  
-              const description = document.createElement('p');
-              description.textContent = photo.description;
-  
-              galleryItem.appendChild(img);
-              galleryItem.appendChild(description);
-              galleryContainer.appendChild(galleryItem);
-            });
-  
-            showSection('gallery');
-          })
-          .catch(error => console.error('Error fetching photos.json:', error));
-      }
-  
-      function navigateGallery(direction) {
-        const galleryItems = document.getElementsByClassName('gallery-item');
-        let currentIndex = Array.from(galleryItems).findIndex(item => item.style.display !== 'none');
-  
-        if (currentIndex !== -1) {
-          galleryItems[currentIndex].style.display = 'none';
-  
-          if (direction === 'next') {
-            currentIndex = (currentIndex + 1) % galleryItems.length;
-          } else if (direction === 'prev') {
-            currentIndex = (currentIndex - 1 + galleryItems.length) % galleryItems.length;
-          }
-  
-          galleryItems[currentIndex].style.display = 'block';
-        }
-      }
-  
-      document.addEventListener('DOMContentLoaded', function () {
-        showSection('about-me');
+      return response.json();
+    })
+    .then(data => {
+      container.innerHTML = '';
+
+      data.photo.forEach((photo, index) => {
+        const galleryItem = document.createElement('div');
+        galleryItem.className = 'gallery-item';
+        galleryItem.style.display = index === 0 ? 'block' : 'none';
+
+        const img = document.createElement('img');
+        img.src = photo.url;
+        img.alt = photo.description;
+
+        const description = document.createElement('p');
+        description.textContent = photo.description;
+
+        galleryItem.appendChild(img);
+        galleryItem.appendChild(description);
+        container.appendChild(galleryItem);
       });
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+function navigateGallery(direction) {
+  const galleryItems = document.getElementsByClassName('gallery-item');
+  let currentIndex = Array.from(galleryItems).findIndex(item => item.style.display !== 'none');
+
+  if (currentIndex !== -1) {
+    galleryItems[currentIndex].style.display = 'none';
+
+    if (direction === 'next') {
+      currentIndex = (currentIndex + 1) % galleryItems.length;
+    } else if (direction === 'prev') {
+      currentIndex = (currentIndex - 1 + galleryItems.length) % galleryItems.length;
+    }
+
+    galleryItems[currentIndex].style.display = 'block';
+  }
+}
+
+document.addEventListener('keydown', function (event) {
+  const gallerySection = document.getElementById('gallerySection');
+
+  if (gallerySection.classList.contains('active-section')) {
+    navigateGalleryByArrow(event.key);
+  }
+});
+
+function navigateGalleryByArrow(key) {
+  if (key === 'ArrowLeft') {
+    navigateGallery('prev');
+  } else if (key === 'ArrowRight') {
+    navigateGallery('next');
+  }
+}
